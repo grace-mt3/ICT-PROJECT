2ac93b874b2e4963573a86d0db73be72330fb3d0
grace-mt3 <gr.taylor@lis.sc.ke>
2026-04-01 19:33:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,22 @@ console.log(`Welcome to ${MyName} !`);
 let MyAge = "2"; 
 console.log(MyAge);
 let IsStudio = true;
-console.log(IsStudio);   
+console.log(IsStudio); 
+let Owner = true;
+console.log(Owner);  
 window.alert(`Welcome to ${MyName} !\nRead the information below.`);
+if (IsStudio) {
+    console.log("this is a studio")
+} else if (Owner) {
+    console.log("you are the owner")
+} else {
+    console.log("this is not a studio and you are not the owner")
+} 
+let UserName = window.prompt("What is your name?"); 
+window.prompt("What is your name?");
+window.alert(`Hello, ${UserName}! Welcome to ${MyName}.`);
+let input;
+document.getElementById("ButtonID").onclick = function() {
+    input = document.getElementById("inputID").value;
+    window.alert(`Hello, ${input}! Welcome to ${MyName}.`);
+}
